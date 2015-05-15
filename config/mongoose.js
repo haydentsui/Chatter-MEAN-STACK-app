58e4145080c04240a48b809543ba448378c9af32
haydentsui <me@haydentsui.ca>
2015-05-15 10:26:30 -0400

    Edited mongoose config

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,6 +4,5 @@ var config = require('./config'),
 module.exports = function() {
      var db = mongoose.connect(config.db);
      require('../app/models/user.server.model');
-	return db; 
-
+	return db;
 };
