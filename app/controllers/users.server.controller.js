453bcc6bd573e1edb996a2141daf0dfa15627fc3
haydentsui <me@haydentsui.ca>
2015-07-09 09:41:43 -0400

    Finished implementing the MVC for articles

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -164,7 +164,17 @@ exports.saveOAuthUserProfile = function(req, profile, done){
 };
 
 
+exports.requiresLogin = function(req, res, next) {
+  // If a user is not authenticated send the appropriate error message
+  if (!req.isAuthenticated()) {
+    return res.status(401).send({
+      message: 'User is not logged in'
+    });
+  }
 
+  // Call the next middleware
+  next();
+};
 
 
 
--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -1,6 +1,6 @@
 angular.module('example').controller('ExampleController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// Get the user's 'fullName' 
-		$scope.name = Authentication.user ? Authentication.user.fullName : 'MEAN Application';
+		$scope.authentication = Authentication;
 	}
 ]);
